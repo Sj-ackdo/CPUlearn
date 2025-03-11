b2504c2507af8a56a1cd21e660778c2f2148d2e5
Sj-ackdo <jackdegreat2015@gmail.com>
2025-03-11 21:43:44 +0100

    final

--- logicgates.py
+++ logicgates.py
@@ -13,5 +13,38 @@ def AND():
         return 0
     elif(rx and ry == 1):
         return 1
+
+def NAND():
+    if(rx and ry == 1):
+        return 0
+    else:
+        return 1
     
+    
+def OR():
+    if(rx and ry == 0):
+        return 0
+    else:
+        return 1
+
+def NOR():
+    if(rx and ry == 0):
+        return 1
+    else:
+        return 0
+  
+def XOR():
+    if(rx == 0 and ry == 1):
+        return 1
+    if(rx == 1 and ry == 0):
+        return 1
+    else:
+        return 0
+    
+def INV():
+    if(rx or ry == 1):
+        return 0
+    if(rx or ry == 0):
+        return 1
+
 print(AND())
