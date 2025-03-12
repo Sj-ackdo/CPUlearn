2abc93d381aa6236e94d9c6bd35b2c26bfa2ca54
8-NabBit <tijnbuijsman07@gmail.com>
2025-03-12 17:15:52 +0100

    arf uhm excuse me we need have a talk

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ def switch(num):
         entries[num].config(text = "1")
     else: entries[num].config(text = "0")
 
-
+def create_window():
+    top_window = Toplevel()
+    e = Label(top_window, text="wah")
+    e.pack
 
 
 
@@ -42,28 +45,40 @@ def switch(num):
 
 
 # GUI stuff
-importButton = Button(root, text="File import", bg=bgColor, justify="center")
+importButton = Button(root, command=create_window, text="File import", bg=bgColor, justify="center")
 importButton.grid(row=0, column=0)
 
 inputLabel = Label(root, text="Instruction", bg=bgColor, justify="center")
 inputLabel.grid(row=1, column=0)
 
-inputrow = 1
-inputcollumn = 1
+registerxLabel = Label(root, text="Register X:", bg=bgColor, justify="center")
+registerxLabel.grid(row=2, column=0)
+registerxvalueLabel = Label(root, text="NULL", bg=bgColor, justify="center")
+registerxvalueLabel.grid(row=2, column=1)
+
+registeryLabel = Label(root, text="Register Y:", bg=bgColor, justify="center")
+registeryLabel.grid(row=2, column=2)
+registeryvalueLabel = Label(root, text="NULL", bg=bgColor, justify="center")
+registeryvalueLabel.grid(row=2, column=3)
+
+# bit input buttons
+framebuttons = Frame(root, bg=bgColor)
+framebuttons.grid(row=1, column=1)
 
 entries = [
-    Button(root, command=lambda:switch(0), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(1), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(2), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(3), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(4), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(5), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(6), width=2, text=0, justify="center"),
-    Button(root, command=lambda:switch(7), width=2, text=0, justify="center")
+    Button(framebuttons, command=lambda:switch(0), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(1), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(2), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(3), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(4), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(5), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(6), width=2, text="0", justify="center"),
+    Button(framebuttons, command=lambda:switch(7), width=2, text="0", justify="center"),
 ]
 
+# put bit input buttons on screen
 for i in range(0, 8):
-    entries[i].grid(row=inputrow, column=(inputcollumn+i))
+    entries[i].pack(side=RIGHT)
 
 # Hou onderaan
 root.mainloop()
