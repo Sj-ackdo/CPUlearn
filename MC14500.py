75bd17883fc07dd7b8cc2a5dff0e617b5eb6162e
Sj-ackdo <jackdegreat2015@gmail.com>
2025-03-12 10:45:43 +0100

    s

--- MC14500.py
+++ MC14500.py
@@ -3,28 +3,58 @@
 #         "0000": ""
 #     }
 #     return av_ins.get(instruction, "not in instructionset")
-instruction = int(0o1)
-global register
+instruction = int()
 register = int()
 Xin = int(1)
 Yin = int()
 
 
 def instructionset(instruction):
-    match instruction:
-        case 0000:
-           pass
-        case 0o1:
-            register = Xin
-        case 0o10:
-            if(Xin == 1):
-                register == 0
-            elif(Xin == 0):
-                register == 1
-        case 0o11:
-            register = register * Xin
-    
+    global register
+    global Xin
+    global Yin
+    if instruction == 0o0: # do nothing
+        register = register
+    elif instruction == 0o1: # Load in RR
+        register = Xin
+    elif instruction == 0o10: # NOT (Store complemenet)
+        if Xin == 0:
+            register = 1
+        elif Xin == 1:
+            register = 0
+    elif instruction == 0o11: # AND
+        register = Xin * register
+    elif instruction == 0o100: # AND Complement
+        if Xin == 1:
+            register = 0 * register
+        elif Xin == 0:
+            register = 1 * register
+    elif instruction == 0o101: # OR
+        if Xin == 1:
+            if register == 1:
+                register = 0
+            elif register == 0:
+                register = 1
+        elif Xin == 0:
+            if register == 1:
+                register = 1
+            elif register == 0:
+                register = 0
+    elif instruction == 0o110:
+        
+
+    else:
+        register = "not in instructionset"
+    return register
+
+# DEBUG
+print("Register:", register)
+print("Xin:", Xin)
+
 instructionset(0o1)
-instructionset(0o11)  
-print(register)
-print(Xin)
+print("Register:", register)
+print("Xin:", Xin)
+
+instructionset(0o11)
+print("Register", register)
+print("Xin", Xin)
