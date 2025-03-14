beb1f487fd0848cacc86f487c79fd22303c530b3
8-NabBit <tijnbuijsman07@gmail.com>
2025-03-14 19:20:00 +0100

    orlando grote penis

--- main.py
+++ main.py
@@ -1,7 +1,96 @@
 # Imports
 from tkinter import *
 import os, sys
-from MC14500 import *
+
+instruction = str()
+register = int(0)
+Xin = int(0)
+Yin = int(0)
+IEN = int(0)
+OEN = int(0)
+JMP = int(0)
+RTN = int(0)
+SKP = int(0) 
+NOPO = int(0)
+NOPF = int(0) 
+def instructionset(instruction):
+    global register; int(register)
+    global Xin
+    global Yin
+    global IEN
+    global OEN
+    global JMP
+    global RTN
+    global SKP
+    global NOPO
+    global NOPF
+    if instruction == "0000": # do nothing
+        register = register
+        if NOPO == 0:
+            NOPO = 1
+        elif NOPO == 1:
+            NOPO = 0
+    elif instruction == "0001": # Load in RR
+        register = Xin
+    elif instruction == "0010": # Store Complement (not)
+        if Xin == 0:
+            register = 1
+        elif Xin == 1:
+            register = 0
+    elif instruction == "0011": # AND
+        register = Xin * register
+    elif instruction == "0100": # AND Complement
+        if Xin == 1:
+            register = 0 * register
+        elif Xin == 0:
+            register = 1 * register
+    elif instruction == "0101": # OR
+        if Xin == 1:
+            if register == 1:
+                register = 0
+            elif register == 0:
+                register = 1
+        elif Xin == 0:
+            if register == 1:
+                register = 1
+            elif register == 0:
+                register = 0
+    elif instruction == "0110": # OR Complement (NOR)
+        if Xin & register == 1:
+            register = 0
+        elif Xin & register == 0:
+            register = 1
+    elif instruction == "0111": # XNOR
+        if register == Xin:
+            register = 1
+        elif register != Xin:
+            register = 0
+    elif instruction == "1000": # Store command (Write)
+        Yin = register
+        Xin = register
+    elif instruction == "1001": # Store Complement (Write)
+        if register == 0:
+            Yin = 1
+            Xin = 1
+        elif register == 1:
+            Yin = 0 
+            Xin = 0
+    elif instruction == "1010": # Input Enable
+        IEN = 1
+    elif instruction == "1011": # Output Enable
+        OEN = 1
+    elif instruction == "1100": # JMP flag
+        JMP = 1
+    elif instruction == "1101": # Return flag
+        RTN = 1
+    elif instruction == "1110": # skip next instruct
+        SKP = 1
+    elif instruction == "1111": # flag f
+        register = register
+        if NOPF == 0:
+            NOPF = 1
+        elif NOPF == 1:
+            NOPF = 0
 
 # Window
 bgColor = "#C3C3C3"
@@ -64,19 +153,31 @@ def get_bit_states():
         bit_states.append(entries[i].config("text")[-1])
         instr = instr + entries[i].config("text")[-1]
 
+    print(bit_states)
+
     Xin = bit_states[len(bit_states)-1]
 
     instr = instr[:-1]
+
+    print(instr)
+    print(Xin)
+
     return instr
 
 
 def clock_cycle():
+    global Yin
 
-    result = instructionset(get_bit_states())
+    input = get_bit_states()
+    print(input)
 
-    if result == 0:
+    instructionset(input)
+
+    print(Yin)
+
+    if Yin == 0:
         outputLEDLabel.config(bg = "#FF0000")
-    elif result == 1:
+    elif Yin == 1:
         outputLEDLabel.config(bg = "#00FF00")
 
 
